24c5acbd60562d95657ec155203d87823f911b1b
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-12-23 15:57:56 -0300

    Matriz gameboard inicial criada

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@
 
 const bomberman = document.querySelector('.bomberman');
 const step = 48;
+var positionX = 0;
+var positionY = 0;
+
+const matrizGameBoard = [
+    [0,1,0,1,0,1,0,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,0,1,0,1,0,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,0,1,1,1,1,1,1,1,0,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0],
+];
 
 // Add event listener on keydown
 document.addEventListener('keydown', (event) => {
@@ -12,7 +27,9 @@ document.addEventListener('keydown', (event) => {
     /* Testa a tecla pressionada */
     if (keyCode == 'ArrowRight') {
         if ((positionLeft + step) <= 624) {
-            bomberman.style.left = `${positionLeft + step}px`
+            if (canWalkX()) {
+                bomberman.style.left = `${positionLeft + step}px`
+            }
         } 
     } else if (keyCode == 'ArrowLeft') {
         if ((positionLeft - step) >= 48) {
@@ -20,7 +37,10 @@ document.addEventListener('keydown', (event) => {
         }
     } else if (keyCode == 'ArrowDown') {
         if ((positionTop + step) <= 432) {
-            bomberman.style.top = `${positionTop + step}px`
+            if (canWalkY()) {
+                bomberman.style.top = `${positionTop + step}px`
+                
+            }
         }
     } else if (keyCode == 'ArrowUp') {
         if ((positionTop - step) >= 0) {
@@ -28,4 +48,25 @@ document.addEventListener('keydown', (event) => {
         }
     }
 
-  }, false);
+}, false);
+
+
+function canWalkX() {
+    console.log(`matrizGameBoard[${positionY}][${positionX}]`)
+    if (matrizGameBoard[positionY][positionX+1] == 0) {
+        positionX++;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function canWalkY() {
+    console.log(matrizGameBoard[positionY][positionX+1])
+    if (matrizGameBoard[positionY+1][positionX] == 0) {
+        positionY++;
+        return true;
+    } else {
+        return false;
+    }
+}
