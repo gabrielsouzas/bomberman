48ff8e53085d3dd64edc021d619e099982b01d04
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-12-27 15:27:29 -0300

    Novas sprites e logicas

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 /* MOVIMENTAÇÃO BOMBERMAN */
 
 const bomberman = document.querySelector('.bomberman');
-const step = 48;
-var positionX = 0;
-var positionY = 0;
+const step = 42;
+var positionX = 1;
+var positionY = 1;
 
-const matrizGameBoard = [
+const initLeft = 84;
+const initTop = 40;
+
+/*const matrizGameBoard = [
     [0,1,0,1,0,1,0,1,0,1,1,1,0],
     [0,0,0,0,0,0,0,0,0,1,1,1,0],
     [0,1,0,1,0,1,0,1,0,1,0,1,0],
@@ -16,6 +19,24 @@ const matrizGameBoard = [
     [1,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,0,1,0,1,0,1,0,1,0,1,1],
     [0,0,0,0,0,0,0,0,0,0,0,0,0],
+];*/
+
+// 13X15
+const matrizGameBoard = [
+//   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],// 0
+    [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],// 1
+    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],// 2
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],// 3
+    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],// 4
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],// 5
+    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],// 6
+    [1,0,0,0,0,0,1,0,0,0,0,0,0,0,1],// 7
+    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],// 8
+    [1,0,1,0,0,0,0,1,0,0,0,0,0,0,1],// 9
+    [1,0,1,0,1,0,1,0,1,0,1,1,1,0,1],// 10
+    [1,0,1,0,0,0,0,0,0,0,0,0,0,0,1],// 11
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],// 12
 ];
 
 // Add event listener on keydown
@@ -26,30 +47,33 @@ document.addEventListener('keydown', (event) => {
     
     /* Testa a tecla pressionada */
     if (keyCode == 'ArrowRight') {
-        if ((positionLeft + step) <= 624) {
+        //if ((positionLeft + step) <= 624) {
             if (canWalkRight()) {
                 bomberman.style.left = `${positionLeft + step}px`
             }
-        } 
+        //} 
     } else if (keyCode == 'ArrowLeft') {
-        if ((positionLeft - step) >= 48) {
+        //if ((positionLeft - step) >= 48) {
             if (canWalkLeft()) {
                 bomberman.style.left = `${positionLeft - step}px`
             }
-        }
+        //}
     } else if (keyCode == 'ArrowDown') {
-        if ((positionTop + step) <= 432) {
+        //if ((positionTop + step) <= 432) {
             if (canWalkDown()) {
                 bomberman.style.top = `${positionTop + step}px`
                 
             }
-        }
+        //}
     } else if (keyCode == 'ArrowUp') {
-        if ((positionTop - step) >= 0) {
+        //if ((positionTop - step) >= 0) {
             if (canWalkUp()) {
                 bomberman.style.top = `${positionTop - step}px`
             }
-        }
+       //}
+    } else if (keyCode == 'KeyX') {
+        /* Deixar a bomba na tela e na matriz */
+        dropBomb();
     }
 
 }, false);
@@ -110,8 +134,8 @@ gameBoard.appendChild(breakableWall);
 breakableWall.style.left = '94px';
 breakableWall.style.top = '69px';*/
 
-createBreakableWall(94,69)
-createBreakableWall(46,117)
+//createBreakableWall(94,69)
+//createBreakableWall(46,117)
 
 function createBreakableWall(positionLeft, positionTop) {
     var breakableWall = document.createElement('div');
@@ -121,4 +145,17 @@ function createBreakableWall(positionLeft, positionTop) {
 
     breakableWall.style.left = `${positionLeft}px`;
     breakableWall.style.top = `${positionTop}px`;
+
+    /* Alocação Matriz */
+    matrizGameBoard[((positionTop-initTop)/step)][((positionLeft-initLeft)/step)] = 2;
+}
+
+dropBomb = () => {
+    console.log('bomba na tela')
+
+    
+    setTimeout(() => {
+        console.log('bomba explode')
+        console.log('remove parede da tela')
+    }, 3000);
 }
